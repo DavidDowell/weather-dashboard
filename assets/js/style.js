b12934dc1d578f5a85d289abbd9a806cee8ecbcf
DavidDowell <daviddowell1@gmail.com>
2022-09-06 11:42:49 -0400

    starting structure

---
+++ assets/js/style.js
@@ -0,0 +1,5 @@
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector('#city-name');
+var currentWeatherEl = document.querySelector('#current-weather');
+var forecastEl = document.querySelector('#forecast');
+
